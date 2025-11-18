e4fb0c411cb6040481470cda429ddff1107f5e65
0255962-star <0255962@up.edu.mx>
2025-11-18 14:14:55 -0600

    Create __init__.py

---
+++ app_core/__init__.py
@@ -0,0 +1,14 @@
+"""Core modules for the Streamlit app orchestrated by app.py."""
+
+__all__ = [
+    "ui_config",
+    "secrets",
+    "sheets_client",
+    "cache_prices",
+    "prices_fetch",
+    "tx_positions",
+    "metrics",
+    "masters",
+    "pages",
+]
+
